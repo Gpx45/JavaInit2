49d062b5f749401cda8be31317b4900904e5700b
Gpx45 <vich.martins@gmail.com>
2017-04-30 19:36:46 -0400

    Added Polumorphic Test

---
+++ PolymorphismTest/PolymorphismTest.java
@@ -0,0 +1,32 @@
+
+public class PolymorphismTest
+{
+  public static void main(String[] args)
+  {
+    // Assign super class reference to superclass variable.
+    CommissionEmployee commissionEmployee = new CommissionEmployee(
+    "Sue", "Jones", "222-22-2222", 10000, .06);
+
+    // assign subclass reference to subclass variable.
+    BasePlusCommissionEmployee basePlusCommissionEmployee = new BasePlusCommissionEmployee(
+    "Bob", "Lewis", "333-33-3333", 5000, .04, 300);
+
+    // invoke toString on superclass object using superclass variable.
+    System.out.printf("%s %s: \n\n%s\n\n",
+    "Call CommisssionEmployee's toString with superclass reference",
+    "to superclass object", commissionEmployee.toString());
+
+    // invoke toString on subclass object using superclass variable.
+
+    System.out.printf("%s %s: \n\n%s\n",
+    "Call BasePlusCommissionEmployee's toString with subclass",
+    "reference to subclass object", basePlusCommissionEmployee.toString());
+
+    // invoke toString on subclass object using subclass variable
+    CommissionEmployee commissionEmployee2 = basePlusCommissionEmployee;
+    System.out.printf("%s %s: \n\n%s\n",
+    "Call BasePlusCommissionEmployee's toString with superclass",
+    "reference to subclass object", commissionEmployee2.toString());
+
+  }
+}
