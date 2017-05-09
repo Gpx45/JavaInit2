8a67a8ce42b76104b43ae6bdd913069603b122b4
Victor <vich.martins@gmail.com>
2017-05-09 16:07:41 -0400

    Added assert ex

---
+++ assertTest/src/assertClass.java
@@ -0,0 +1,19 @@
+/**
+ * Created by Victor on 5/9/2017.
+ */
+
+import java.util.Scanner;
+
+public class assertClass {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Please Enter a number: ");
+        int number = input.nextInt();
+
+        // typically assert is disabled so you must explicitly use it either in debugging or in command line.
+        assert(number <= 0 && number >= 10);
+        System.out.printf("You've entered %d\n", number);
+
+    }
+}
